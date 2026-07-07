f37b17387dee33d5a68eb7d88db540cffeeb95a1
raneemgaby <raneemjibreel85@gmail.com>
2026-07-07 13:25:18 +0300

    Implement binary search for guessing number

---
+++ 15-Guess-Number-Higher-Or-Lower/Solution.java
@@ -0,0 +1,16 @@
+public class Solution extends GuessGame{
+    public int guessNumber(int n){
+        int low=1;
+        int high=n;
+        if(1 <= n && n <=(Math.pow(2,31)-1)){
+            while(low <= high){
+                int mid=low+(high-low)/2;
+                int result=guess(mid);
+                if(result==0){return mid;}
+                else if(result==-1){ high=mid-1;}
+                else{low=mid+1;}
+            }
+        }
+        return -1;
+    }
+}
